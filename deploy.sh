f5423926a38d1c0fa8318484fb6956aaedbe4a18
Dan Ackerson <dan@ackerson.de>
2020-03-02 15:35:22 +0100

    correct implementation

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 # sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 # sudo reboot
 #NOTE2: docker stop traefik !! otherwise, pain & confusion
-
-export INTERNAL_HOST_IP=`hostname --ip-address` # for patching 02-svcs.yml -> externalIPs!
 curl -sfL https://get.k3s.io | sh -s - server --no-deploy traefik --no-deploy servicelb
 sudo kubectl apply -f 01-clusterrole-CRDs.yml
 sudo kubectl apply -f 02-svcs-middleware-secrets.yml
