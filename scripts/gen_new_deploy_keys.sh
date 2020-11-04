49236400029b129b7aa2dd920859032703ba137d
danackerson <dan@ackerson.de>
2020-11-04 11:27:53 +0100

    prepare my-ca

--- scripts/gen_new_deploy_keys.sh
+++ scripts/gen_new_deploy_keys.sh
@@ -8,16 +8,15 @@ else
     exit
 fi
 
-rm $WORKING_DIR/id_ed25519_github_deploy*
+rm -f $WORKING_DIR/id_ed25519_github_deploy*
 
 ssh-keygen -t ed25519 -a 100 -f $WORKING_DIR/id_ed25519_github_deploy -q -N ""
-ssh-keygen -s ca_key -I github -n ubuntu,ackersond -P "$CACERT_KEY_PASS" -V +4w -z $(date +%s) $WORKING_DIR/id_ed25519_github_deploy
+ssh-keygen -s $WORKING_DIR/ca_key -I github -n ubuntu,ackersond -P "$CACERT_KEY_PASS" -V +4w -z $(date +%s) $WORKING_DIR/id_ed25519_github_deploy
 CERT_INFO=`ssh-keygen -L -f $WORKING_DIR/id_ed25519_github_deploy-cert.pub`
 
 # heavy lifting which updates github secrets via API
 if $WORKING_DIR/github_deploy_secrets.py ; then
     rm $WORKING_DIR/id_ed25519_github_deploy*
-    #TODO kick off deploy of bender-slackbot
 
     curl -s -F token=$SLACK_API_TOKEN -F channel=C092UE0H4 \
         -F text="$HOSTNAME just updated the SSH deploy key and cert in Org Secrets\n: $CERT_INFO" https://slack.com/api/chat.postMessage
