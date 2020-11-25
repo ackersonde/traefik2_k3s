31310e71e99f14276eb9347871ac8a03dcbb062a
danackerson <dan@ackerson.de>
2020-11-25 17:21:32 +0100

    lock traefik to pi4

--- scripts/gen_new_deploy_keys.sh
+++ scripts/gen_new_deploy_keys.sh
@@ -8,7 +8,7 @@ else
     exit
 fi
 
-rm -f $WORKING_DIR/id_ed25519_github_deploy* $WORKING_DIR/ghActions.conf
+rm -f $WORKING_DIR/id_ed25519_github_deploy* $WORKING_DIR/ghActions.conf $WORKING_DIR/ghActions.pub
 
 ssh-keygen -t ed25519 -a 100 -f $WORKING_DIR/id_ed25519_github_deploy -q -N ""
 ssh-keygen -s $WORKING_DIR/ca_key -I github -n ubuntu,ackersond -P "$CACERT_KEY_PASS" -V +5w -z $(date +%s) $WORKING_DIR/id_ed25519_github_deploy
@@ -23,12 +23,18 @@ sudo kubectl scale deployment --replicas=0 wireguard && sudo kubectl scale deplo
 # capture new peer config in file
 sudo kubectl exec -it $(sudo kubectl get po | grep wireguard | awk '{print $1; exit}' | tr -d \\n) -- cat /config/peer_githubActions/peer_githubActions.conf | tee $WORKING_DIR/ghActions.conf
 
-NEW_WG_PUBKEY=`cat $WORKING_DIR/ghActions.conf | grep Public`
+sudo kubectl exec -it $(sudo kubectl get po | grep wireguard | awk '{print $1; exit}' | tr -d \\n) -- cat /config/peer_githubActions/publickey-peer_githubActions | tee $WORKING_DIR/ghActions.pub
+
+NEW_WG_PUBKEY=`cat $WORKING_DIR/ghActions.pub`
 
 # heavy lifting which updates github secrets via API
 if $WORKING_DIR/github_deploy_secrets.py ; then
-    rm $WORKING_DIR/id_ed25519_github_deploy* $WORKING_DIR/ghActions.conf
+    rm $WORKING_DIR/id_ed25519_github_deploy* $WORKING_DIR/ghActions.conf $WORKING_DIR/ghActions.pub
 
-    curl -s -F token=$SLACK_API_TOKEN -F channel=C092UE0H4 \
-        -F text="$HOSTNAME just updated the SSH deploy key, cert & wireguard keys in Org Secrets\n: $CERT_INFO\nWireguard client $NEW_WG_PUBKEY" https://slack.com/api/chat.postMessage
+    SLACK_URL=https://slack.com/api/chat.postMessage
+    curl -s -d token=$SLACK_API_TOKEN -d channel=C092UE0H4 \
+        -d text="$HOSTNAME just updated the SSH deploy key & cert in Org Secrets:" $SLACK_URL
+    curl -s -d token=$SLACK_API_TOKEN -d channel=C092UE0H4 -d text="$CERT_INFO" $SLACK_URL
+    curl -s -d token=$SLACK_API_TOKEN -d channel=C092UE0H4 \
+        -d text="New wireguard client public key: $NEW_WG_PUBKEY" $SLACK_URL
 fi
